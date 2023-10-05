becd27e479c08279215e74b12975a77042840e86
BiteDragon <himanshusrivastav156@gmail.com>
2023-10-05 15:27:28 +0530

    Boat.java updated

--- src/main/java/com/driver/Boat.java
+++ src/main/java/com/driver/Boat.java
@@ -32,3 +32,24 @@
 //    }
 //}
 
+package com.driver;
+
+public class Boat implements WaterVehicle{
+    public Boat(String name, int capacity) {
+        this.name = name;
+        this.capacity = capacity;
+    }
+
+    private String name="The Black Pearl";
+    private int capacity=500;
+    @Override
+    public String getVehicleName() {
+        return name;
+    }
+
+    @Override
+    public int getVehicleCapacity() {
+        return capacity;
+    }
+
+}
